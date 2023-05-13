66fad814adb6cdc04f22793646306aece2f3e3a2
avkotau <kotov.alexander1@mail.ru>
2023-05-13 20:03:55 +0200

    additional task №1 completed

---
+++ src1/Styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+      width: 100px;
+      height: 30px;
+      background: red;
+      border-radius: 5px;
+      border: none;
+      color: white;
+      margin: 5px;
+    `
+export const TomatoButton = styled(Button)`
+      background: tomato;
+    `
+export const BlueButton = styled(Button)`
+      background: blue;
+    `
+
